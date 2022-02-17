2002f934a3fc9552e1d9b85b3d91613bee4ab981
Xzenooo <e.jouny@lprs.fr>
2022-02-17 23:03:14 +0100

    refactor: elu economique

--- src/Personne/Economique.java
+++ src/Personne/Economique.java
@@ -1,4 +1,25 @@
 package Personne;
 
-public class Economique {
+public class Economique extends Elu {
+
+    public Economique(String nom, String prenom) {
+        super(nom, prenom);
+    }
+
+    public String verserDotation(Personne monAssistant, int montant){
+        if (this.assistant == this) {
+            int montantDistribuer = 0;
+            if (montant > 1480){
+                montantDistribuer = 1480;
+            }
+            else{
+                montantDistribuer = montant;
+            }
+            this.assistant.addSous(montantDistribuer);
+            return "Vous avez verser "+montantDistribuer+"€ à "+monAssistant;
+        }
+        else{
+            return "Vous ne pouvez pas verser de dotation à "+monAssistant+" car ce n'est pas votre assistant";
+        }
+    }
 }
